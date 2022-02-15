11718f04baa56076cb09d62ebe11e357b3545753
Polina Dudchenko <pv.dudchenko@gmail.com>
2022-02-15 18:49:59 +0100

    pagination done

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,11 +2,13 @@ const { Contact } = require('../../models')
 
 const listContacts = async (req, res, next) => {
   try {
+    const { page = 1, limit = 20 } = req.query
     const { _id } = req.user
-    const result = await Contact.find(
-      { owner: _id },
-      '-createdAt -updatedAt',
-    ).populate('owner', 'email')
+    const skip = (page - 1) * limit
+    const result = await Contact.find({ owner: _id }, '-createdAt -updatedAt', {
+      skip,
+      limit: +limit,
+    }).populate('owner', 'email')
     res.json(result)
   } catch (error) {
     next(error)
