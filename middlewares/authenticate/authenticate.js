b69f64093c1dde9d90849c9793bfe2fc41b09671
Polina Dudchenko <pv.dudchenko@gmail.com>
2022-02-15 13:10:40 +0100

    midllware authenticate done

--- middlewares/authenticate/authenticate.js
+++ middlewares/authenticate/authenticate.js
@@ -1,3 +1,30 @@
-const authenticate = async (req, res, next) => {}
+const CreateError = require('http-errors')
+const jwt = require('jsonwebtoken')
+const { User } = require('../../models')
+
+const { SECRET_KEY } = process.env
+
+const authenticate = async (req, res, next) => {
+  try {
+    const { authenticate = ' ' } = req.headers
+    const [bearer, token] = authenticate.split(' ')
+    if (bearer !== 'Bearer') {
+      throw new CreateError(401, 'Not authorized')
+    }
+    const { id } = jwt.verify(token, SECRET_KEY)
+    const user = User.findById(id)
+    if (!user || !user.token) {
+      throw new CreateError(401, 'Not authorized')
+    }
+    req.user = user
+    next()
+  } catch (error) {
+    if (!error.status) {
+      error.status = 401
+      error.message = 'Not authorized'
+    }
+    next(error)
+  }
+}
 
 module.exports = authenticate
