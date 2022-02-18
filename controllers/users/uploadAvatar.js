73b5486f31871951245c3ddebd2e73778d416fc4
Polina Dudchenko <pv.dudchenko@gmail.com>
2022-02-18 14:10:28 +0100

    save user avatar

--- controllers/users/uploadAvatar.js
+++ controllers/users/uploadAvatar.js
@@ -1,10 +1,21 @@
 const fs = require('fs/promises')
 const path = require('path')
+const User = require('../../models/user')
 
 const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars')
 const uploadAvatar = async (req, res, next) => {
+  const { _id } = req.user
+  const { path: tempUpload, filename } = req.file
   try {
-    console.log('aaa')
+    const [extension] = filename.split('.').reverse()
+    const newFileName = `${_id}.${extension}`
+    const resultUpload = path.join(avatarsDir, newFileName)
+    await fs.rename(tempUpload, resultUpload)
+    const avatarURL = path.join('avatars', newFileName)
+    await User.findByIdAndUpdate(_id, { avatarURL })
+    res.json({
+      avatarURL,
+    })
   } catch (error) {
     next(error)
   }
